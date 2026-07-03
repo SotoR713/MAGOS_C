ee5f9a94701effea833f3d5431f63da2a64dd3a8
SotoR713 <Soto_713@hotmail.com>
2026-07-03 09:21:38 -0700

    Agregar archivos de proyecto.

---
+++ MAGOS_C/BACK/Clases/Jefe.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAGOS_C.BACK.Clases
+{
+    internal class Jefe
+    {
+    }
+}
---
+++ MAGOS_C/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MAGOS_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
