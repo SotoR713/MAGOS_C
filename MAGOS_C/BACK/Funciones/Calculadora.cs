7967c7866b8d37b27292241ee6bd9183c151de7a
SotoR713 <Soto_713@hotmail.com>
2026-07-03 11:13:19 -0700

    Refactoriza Calculadora: limpia usings y corrige lógica
    Se eliminan los using innecesarios y se ajusta la declaración del espacio de nombres. Además, se mueve la inicialización de la variable suma dentro del bucle while en el método RaizDigital para corregir su funcionamiento.

--- MAGOS_C/BACK/Funciones/Calculadora.cs
+++ MAGOS_C/BACK/Funciones/Calculadora.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MAGOS_C.BACK.Funciones
+﻿namespace MAGOS_C.BACK.Funciones
 {
     internal class Calculadora
     {
 
         public static int RaizDigital(int numero)
         {
-            int suma = 0;
             while (numero >= 10)
             {
+            int suma = 0;
                 foreach (char i in numero.ToString())
                     suma += int.Parse(i.ToString());
                 numero = suma;
