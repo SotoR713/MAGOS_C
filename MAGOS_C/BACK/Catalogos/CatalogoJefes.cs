8d92b1314702021692d5893c6d30109dfe593800
SotoR713 <Soto_713@hotmail.com>
2026-07-12 16:44:14 -0600

    cambio  de jefes de diccionarios a lista o catalogo (falta crear clase mago)

--- MAGOS_C/BACK/Catalogos/CatalogoJefes.cs
+++ MAGOS_C/BACK/Catalogos/CatalogoJefes.cs
@@ -1,26 +1,19 @@
 ﻿using MAGOS_C.BACK.Modelos;
 
 namespace MAGOS_C.BACK.Catalogos
-{  
+{
     internal class CatalogoJefes
     {
-        public static readonly Dictionary<int, string, Elemento, int, int, int, int, int, int> ValoracionCaracter = new()
-        {
-
-
-            { 0, "Nullizador", Neutral, 28, 28, 2, 4, 1, 0},
-            {1, "Hexecutor", Fuego, 16, 16, 10, 4, 5, 0},
-            {2, "Daemonus", Fuego, 19, 19, 7, 4, 5, 0},
-            {3, "ElProfeCaguamo", Tierra, 22, 22, 6, 6, 1, 0},
-            {4, "Bytemaster", Agua, 18, 18, 5, 3, 9, 0},
-            {5, "Compilator", Planta, 24, 24, 4, 5, 2, 0},
-            {6, "Piri", Neutral, 25, 25, 7, 2, 1, 0},
-            {7, "Fatalerror", Neutral, 20, 20, 8, 3, 4, 0},
-            {8, "CeszarW", Fuego, 17, 17, 7, 5, 6, 0},
-            {9, "CarluxSanguis", Neutral, 26, 26, 6, 2, 1, 0},
-}
-        
-
-
+        public static readonly Jefe Nullizador = new Jefe(0, "Nullizador", Neutral, 28, 28, 2, 4, 1, 0);
+        public static readonly Jefe Hexecutor = new Jefe (1, "Hexecutor", Fuego, 16, 16, 10, 4, 5, 0);
+        public static readonly Jefe Daemonus = new Jefe(2, "Daemonus", Fuego, 19, 19, 7, 4, 5, 0);
+        public static readonly Jefe ElProfeCaguamo=new Jefe(3, "ElProfeCaguamo", Tierra, 22, 22, 6, 6, 1, 0);
+        public static readonly Jefe Bytemaster = new Jefe(4, "Bytemaster", Agua, 18, 18, 5, 3, 9, 0);
+        public static readonly Jefe Compilator = new Jefe(5, "Compilator", Planta, 24, 24, 4, 5, 2, 0);
+        public static readonly Jefe Piri = new Jefe(6, "Piri", Neutral, 25, 25, 7, 2, 1, 0);
+        public static readonly Jefe Fatalerror = new Jefe(7, "Fatalerror", Neutral, 20, 20, 8, 3, 4, 0);
+        public static readonly Jefe CeszarW = new Jefe(8, "CeszarW", Fuego, 17, 17, 7, 5, 6, 0);
+        public static readonly Jefe CarluxSanguis = new Jefe(9, "CarluxSanguis", Neutral, 26, 26, 6, 2, 1, 0);
     }
 }
+
