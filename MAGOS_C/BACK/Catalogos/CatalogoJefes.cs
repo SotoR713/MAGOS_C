06ba36c1b96b52a7e312e4a754d5467fd9a89d1c
SotoR713 <Soto_713@hotmail.com>
2026-07-12 15:39:00 -0600

    creacion de catalogo de jefes

---
+++ MAGOS_C/BACK/Catalogos/CatalogoJefes.cs
@@ -0,0 +1,26 @@
+﻿using MAGOS_C.BACK.Modelos;
+
+namespace MAGOS_C.BACK.Catalogos
+{  
+    internal class CatalogoJefes
+    {
+        public static readonly Dictionary<int, string, Elemento, int, int, int, int, int, int> ValoracionCaracter = new()
+        {
+
+
+            { 0, "Nullizador", Neutral, 28, 28, 2, 4, 1, 0},
+            {1, "Hexecutor", Fuego, 16, 16, 10, 4, 5, 0},
+            {2, "Daemonus", Fuego, 19, 19, 7, 4, 5, 0},
+            {3, "ElProfeCaguamo", Tierra, 22, 22, 6, 6, 1, 0},
+            {4, "Bytemaster", Agua, 18, 18, 5, 3, 9, 0},
+            {5, "Compilator", Planta, 24, 24, 4, 5, 2, 0},
+            {6, "Piri", Neutral, 25, 25, 7, 2, 1, 0},
+            {7, "Fatalerror", Neutral, 20, 20, 8, 3, 4, 0},
+            {8, "CeszarW", Fuego, 17, 17, 7, 5, 6, 0},
+            {9, "CarluxSanguis", Neutral, 26, 26, 6, 2, 1, 0},
+}
+        
+
+
+    }
+}
